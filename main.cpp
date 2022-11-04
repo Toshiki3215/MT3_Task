c05e975bca967cb33755c460c98e4db5931d5335
HatouToshiki <k021g1448@g.neec.ac.jp>
2022-11-04 11:06:12 +0900

    スプライン曲線

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "DxLib.h"
 #include "global.h"
+#include <vector>
 #include "Vector3.h"
 
 int SetCameraPositionAndTargetAndUpVec
@@ -16,6 +17,8 @@ int DrawLine3D(const Vector3& Pos1, const Vector3& Pos2, const unsigned int Colo
 
 //x,y,z 軸の描画
 void DrawAxis3D(const float length);
+
+Vector3 splinePosition(const std::vector<Vector3>& points, size_t startIndex, float t);
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine,
                    _In_ int nCmdShow) {
@@ -60,10 +63,9 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 
 	SetCameraPositionAndTargetAndUpVec
 	(
-		//Vector3(0.0f, 0.0f, -120.0f), //カメラ位置
-		Vector3(0.0f, 200.0f, 0.0f), //カメラ位置
-		Vector3(0.0f, 0.0f, 0.0f),  //カメラの注視点
-		Vector3(0.0f, 0.0f, 1.0f)   //カメラの上の向き
+		Vector3(-20.0f, 20.0f, -200.0f), //カメラ位置
+		Vector3(  0.0f,  0.0f,    0.0f),  //カメラの注視点
+		Vector3(  0.0f,  1.0f,    0.0f)   //カメラの上の向き
 	);
 
 	//時間経過に必要なデータ
@@ -73,15 +75,19 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 
 	//補間で使うデータ
 	//　start → end を 5 [s]で完成させる
-	Vector3 p0(-100.0f, 0.0f, 0.0f); //スタート地点
-	Vector3 p1(0.0f, 0.0f, +100.0f); //制御点
-	Vector3 p2(0.0f, 0.0f, -100.0f); //制御点
-	Vector3 p3(+100.0f, 0.0f, 0.0f); //エンド地点
+	Vector3 p0(-100.0f,   0.0f,   0.0f); //スタート地点
+	Vector3 p1( -50.0f,  50.0f, +50.0f); //制御点
+	Vector3 p2( +50.0f, -30.0f, -50.0f); //制御点
+	Vector3 p3(+100.0f,   0.0f,   0.0f); //エンド地点
 	float maxTime = 5.0f;
 	float timeRate;
 
 	//球の位置
 	Vector3 position;
+
+	std::vector<Vector3>points{ p0,p0,p1,p2,p3,p3 };
+
+	size_t startIndex = 1;
 
 	//実行前にカウンタ値を取得
 	startCount = GetNowHiPerformanceCount(); // long long int型 64bit int
@@ -106,6 +112,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 		if (CheckHitKey(KEY_INPUT_R))
 		{
 			startCount = GetNowHiPerformanceCount();
+			startIndex = 1;
 		}
 
 		//経過時間(elapsedTime [s] )の計算
@@ -113,19 +120,23 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 		elapsedCount = nowCount - startCount;
 		float elapsedTime = static_cast<float>(elapsedCount) / 1000000.0f;
 
-		timeRate = min(elapsedTime / maxTime, 1.0f);
+		timeRate = elapsedTime / maxTime;
 
-		Vector3 a = lerp(p0, p1, timeRate);
+		if (timeRate >= 1.0f)
+		{
+			if (startIndex < points.size() - 3)
+			{
+				startIndex++;
+				timeRate -= 1.0f;
+				startCount = GetNowHiPerformanceCount();
+			}
+			else
+			{
+				timeRate = 1.0f;
+			}
+		}
 
-		Vector3 b = lerp(p1, p2, timeRate);
-
-		Vector3 c = lerp(p2, p3, timeRate);
-
-		Vector3 ab = lerp(a, b, timeRate);
-
-		Vector3 bc = lerp(b, c, timeRate);
-
-		position = lerp(ab, bc, timeRate);
+		position = splinePosition(points, startIndex, timeRate);
 
 		// 描画処理
 		//画面を消去
@@ -135,21 +146,23 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 		DrawAxis3D(500.0f);
 
 		//球の描画
+		DrawSphere3D(p0, 2.0f, 32, GetColor(0, 255, 0), GetColor(255, 255, 255), TRUE);
+		DrawSphere3D(p1, 2.0f, 32, GetColor(0, 255, 0), GetColor(255, 255, 255), TRUE);
+		DrawSphere3D(p2, 2.0f, 32, GetColor(0, 255, 0), GetColor(255, 255, 255), TRUE);
+		DrawSphere3D(p3, 2.0f, 32, GetColor(0, 255, 0), GetColor(255, 255, 255), TRUE);
+
 		DrawSphere3D(position, 5.0f, 32, GetColor(255, 0, 0), GetColor(255, 255, 255), TRUE);
 
-		DrawSphere3D(p0, 3.0f, 32, GetColor(0, 255, 0), GetColor(255, 255, 255), TRUE);
-		DrawSphere3D(p1, 3.0f, 32, GetColor(0, 255, 0), GetColor(255, 255, 255), TRUE);
-		DrawSphere3D(p2, 3.0f, 32, GetColor(0, 255, 0), GetColor(255, 255, 255), TRUE);
-		DrawSphere3D(p3, 3.0f, 32, GetColor(0, 255, 0), GetColor(255, 255, 255), TRUE);
-		
-		DrawFormatString(0, 0, GetColor(255, 255, 255), "position (%6.1f,%6.1f,%6.1f)", position.x, position.y, position.z);
+		DrawFormatString(0,  0, GetColor(255, 255, 255), "position (%5.1f,%5.1f,%5.1f)", position.x, position.y, position.z);
+		DrawFormatString(0, 20, GetColor(255, 255, 255), "startIndex %d", startIndex);
+		DrawFormatString(0, 40, GetColor(255, 255, 255), "timeRate %7.3f [%%]", timeRate);
+		DrawFormatString(0, 60, GetColor(255, 255, 255), "elapedTime %7.3f [s]", elapsedTime);
+		DrawFormatString(0, 80, GetColor(255, 255, 255), "[R] : Restart");
 
-		DrawFormatString(0, 20, GetColor(255, 255, 255), "%7.3f [s]", elapsedTime);
-		DrawFormatString(0, 40, GetColor(255, 255, 255), "[R] : Restart");
-
-		DrawFormatString(0,  60, GetColor(255, 255, 255), "p0(%6.1f,%6.1f,%6.1f)", p0.x, p0.y, p0.z);
-		DrawFormatString(0,  80, GetColor(255, 255, 255), "p1(%6.1f,%6.1f,%6.1f)", p1.x, p1.y, p1.z);
-		DrawFormatString(0, 100, GetColor(255, 255, 255), "p2(%6.1f,%6.1f,%6.1f)", p2.x, p2.y, p2.z);
+		DrawFormatString(0, 100, GetColor(255, 255, 255), "p0 (%6.1f,%6.1f,%6.1f)", p0.x, p0.y, p0.z);
+		DrawFormatString(0, 120, GetColor(255, 255, 255), "p1 (%6.1f,%6.1f,%6.1f)", p1.x, p1.y, p1.z);
+		DrawFormatString(0, 140, GetColor(255, 255, 255), "p2 (%6.1f,%6.1f,%6.1f)", p2.x, p2.y, p2.z);
+		DrawFormatString(0, 160, GetColor(255, 255, 255), "p3 (%6.1f,%6.1f,%6.1f)", p3.x, p3.y, p3.z);
 
 		//---------  ここまでにプログラムを記述  ---------//
 		// (ダブルバッファ)裏面
@@ -210,4 +223,23 @@ void DrawAxis3D(const float length)
 	DrawLine3D(Vector3(-length, 0, 0), Vector3(+length, 0, 0), GetColor(255, 0, 0));
 	DrawLine3D(Vector3(0, -length, 0), Vector3(0, +length, 0), GetColor(0, 255, 0));
 	DrawLine3D(Vector3(0, 0, -length), Vector3(0, 0, +length), GetColor(0, 0, 255));
+}
+
+Vector3 splinePosition(const std::vector<Vector3>& points, size_t startIndex, float t)
+{
+	size_t n = points.size() - 2;
+
+	if (startIndex > n)return points[n];
+	if (startIndex < 1)return points[1];
+
+	Vector3 p0 = points[startIndex - 1];
+	Vector3 p1 = points[startIndex];
+	Vector3 p2 = points[startIndex + 1];
+	Vector3 p3 = points[startIndex + 2];
+
+	Vector3 position = 0.5f * (2 * p1 + (-p0 + p2) * t +
+					          (2 * p0 - 5 * p1 + 4 * p2 - p3) * (t * t) +
+		                      (-p0 + 3 * p1 - 3 * p2 + p3) * (t * t * t));
+
+	return position;
 }
